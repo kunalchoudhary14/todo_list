9943530d38490455d7a267b14fc3d9169fc57693
Kunal choudhary <103172911+kunalchoudhary14@users.noreply.github.com>
2024-05-25 15:34:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+import React  from 'react';
+
+function App() {
+  return (
+    <div lassName="App">
+    <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
